f58da0f3f23a3260bca9176baf319a17d773b015
qin <15739559478@163.com>
2017-03-28 21:55:03 +0800

    自动更新信息

--- app/src/main/java/com/example/qtm/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/qtm/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.qtm.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.qtm.coolweather.gson.Forecast;
 import com.example.qtm.coolweather.gson.Weather;
+import com.example.qtm.coolweather.service.AutoUpdateService;
 import com.example.qtm.coolweather.util.HttpUtil;
 import com.example.qtm.coolweather.util.Utility;
 
@@ -147,6 +149,9 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            //Intent intent = new Intent(this,AutoUpdateService.class);
+                          //  startService(intent);
+
                         }else {
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
@@ -193,6 +198,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        //Intent intent = new Intent(this,AutoUpdateService.class);
+       // startService(intent);
     }
 
     /*加载必应每日一图*/
