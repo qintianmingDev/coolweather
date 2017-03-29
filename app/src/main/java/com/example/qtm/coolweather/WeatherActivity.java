8765d35c5b4175e393bf82eecded021254e2dfa2
qin <15739559478@163.com>
2017-03-29 14:43:30 +0800

    修改图标和名称

--- app/src/main/java/com/example/qtm/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/qtm/coolweather/WeatherActivity.java
@@ -149,8 +149,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
-                            //Intent intent = new Intent(this,AutoUpdateService.class);
-                          //  startService(intent);
+                           // Intent intent = new Intent(this,AutoUpdateService.class);
+                           // startService(intent);
 
                         }else {
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
@@ -199,8 +199,8 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
 
-        //Intent intent = new Intent(this,AutoUpdateService.class);
-       // startService(intent);
+    //   Intent intent = new Intent(this,AutoUpdateService.class);
+    //   startService(intent);
     }
 
     /*加载必应每日一图*/
